239e5ea9b64a16eac80d9507654df33a0494aec2
ChMubasharAli <muhammadmubashirali075@gmail.com>
2024-05-29 14:36:54 +0500

    optimise data base connection

--- src/helper/connectDB.js
+++ src/helper/connectDB.js
@@ -1,27 +1,31 @@
 import mongoose from "mongoose";
 
-export async function connectDB() {
-    try {
-        mongoose.connect(process.env.MONGO_URL, {
-            dbName: "counterfeit"
-        });
-        const connection = mongoose.connection;
 
-        connection.on('connected', () => {
-            console.log('MongoDB connected successfully');
-        })
 
-        connection.on('error', (err) => {
-            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
-            process.exit();
-        })
+const connection = {}
 
-    } catch (error) {
-        console.log('Something goes wrong!');
-        console.log(error);
+async function connectDB() {
 
+    // check the database alredy connected or not 
+    if (connection.isConnected) {
+        console.log("Already connected to database");
+        return;
     }
 
+    try {
+        const db = await mongoose.connect(process.env.MONGO_URL, {
+            dbName: "counterfeit"
+        });
+
+        connection.isConnected = db.connections[0].readyState
+
+        // NOTE: must console.log "db" and "db.connections" you will understand the flow 
+
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.log("Database connection error :: ", error);
+        process.exit(1);
+    }
 }
 
 export default connectDB;
